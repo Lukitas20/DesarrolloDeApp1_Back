04483e0504a1de23c3fdb7ff3847cf908faba745
bauRoccella <broccella@uade.edu.ar>
2025-04-06 18:30:57 -0300

    route change

--- src/main/java/com/example/TpDA1/controller/RouteController.java
+++ src/main/java/com/example/TpDA1/controller/RouteController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/routes")
+@RequestMapping("/routes")
 @RequiredArgsConstructor
 public class RouteController {
     private final RouteService routeService;
